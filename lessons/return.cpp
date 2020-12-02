e4526d464d16d16fddd7787a4f2a6927262a67c5
sheepman39 <jsalata90@gmail.com>
2020-12-02 14:31:48 -0600

    Finished return lesson

---
+++ lessons/return.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// double returns a double
+// double num is the parameter
+double cube(double num){
+
+    return result = num * num * num;
+
+}
+
+int main()
+{
+
+    // can set a return value as a variable 
+    // or just use it
+    double answer = cube(5.0);
+    cout << answer << endl;
+    cout << cube(5.0) << endl;
+
+    return 0;
+}
+
