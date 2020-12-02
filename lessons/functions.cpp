257917f4f4cda38f5868601c264e47683cefdc15
sheepman39 <jsalata90@gmail.com>
2020-12-02 14:30:58 -0600

    Finished functions lessons

--- lessons/functions.cpp
+++ lessons/functions.cpp
@@ -2,9 +2,25 @@
 
 using namespace std;
 
+// function stubs allow you to call a function
+// before it was created
+void sayHi(string name, int age);
+
 int main()
 {
-
-    
+    // passing parameters
+    sayHi("John",42);
     return 0;
+}
+
+// functions require a return type
+// (int, void, ect.)
+// void means that it will return no value
+// parameters require a type then name
+
+void sayHi(string name, int age){
+    
+    cout << "Hello " << name << endl;
+    cout << "You are " << age << " years old" << endl;
+
 }
