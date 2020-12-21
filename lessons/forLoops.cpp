ed7be1e45306598122802feaf11ddce2938fb9bf
sheepman39 <jsalata90@gmail.com>
2020-12-21 14:21:41 -0600

    Completed For Loop Lesson

---
+++ lessons/forLoops.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // for loops work exactly how you would expect
+    // just like java, but declare the variable type
+    for (int i = 1; i <= 5; i++)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
