12fc9e7595b568ba70a8987794b90239f139bff7
sheepman39 <jsalata90@gmail.com>
2021-01-18 14:55:08 -0600

    Finished lesson on object functions

---
+++ lessons/objectFunction.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+    public:
+        string name;
+        string major;
+        double gpa;
+        Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+        }
+        // creates an object function
+        bool hasHonors()
+        {
+            if (gpa >= 3.5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+};
+
+int main()
+{
+
+    Student student1("Jim", "Business", 2.4);
+    Student student2("Pam", "Art", 3.6);
+
+    cout << student1.hasHonors() << endl;
+    cout << student2.hasHonors() << endl;
+
+    return 0;
+}
